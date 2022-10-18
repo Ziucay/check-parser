94fd19af67affc9c6ed535bd83db28f3c2b6019b
ziucay <justlumajustmail@gmail.com>
2022-10-18 20:29:41 +0300

    modify sample page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,11 +1,17 @@
 <h1>Welcome to Cost Accountant!</h1>
-<button onclick="basicButton()">Press me!</button>
-<p id="paragraph"></p>
-
-
-<script>
+<script lang="ts">
     async function basicButton() {
         let response = await fetch("http://localhost:8080/mock/basic")
-        document.getElementById("paragraph").innerText = await response.text()
+        let data: string = await (await response.text())
+
+        return `List: ${data}`
     }
 </script>
+
+{#await basicButton()}
+    <p>Waiting for a fact...</p>
+{:then data}
+    <p>{@html data}</p>
+{:catch error}
+    <p>An error occurred!</p>
+{/await}
