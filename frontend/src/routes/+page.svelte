dae70de6ca4022d73a67577a6d0f6b6657ccaae7
Azat Bariev <justlumajustmail@gmail.com>
2022-10-19 15:38:13 +0300

    try client-side first

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,12 +1,28 @@
-<h1>Welcome to Cost Accountant!</h1>
-<script lang="ts">
-    import { load } from './+page.server'
+<script lang="ts" context="module">
+    export async function load({ fetch }) {
+        const response = await fetch("http://localhost:8080/mock/basic")
+        let data: string = await (await response.text())
+
+        if (response.ok) {
+            return {
+                props: {
+                    data
+                }
+            }
+        }
+
+        return {
+            status: response.status,
+            error: new Error("Couldn't get receipt list")
+        }
+    }
 </script>
 
-{#await load}
-    <p>Waiting for a data...</p>
-{:then data}
-    <p>{@html load}</p>
-{:catch error}
-    <p>An error occurred!</p>
-{/await}
+<script lang="ts">
+    export let data
+</script>
+
+
+<h1>Welcome to Cost Accountant!</h1>
+
+<p>{data}</p>
