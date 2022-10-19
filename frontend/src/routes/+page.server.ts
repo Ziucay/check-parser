3fd16f3b2b8a354253fa15f91fa896b13eb5f84d
Azat Bariev <justlumajustmail@gmail.com>
2022-10-19 19:16:54 +0300

    change api url to own api

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -1,7 +1,7 @@
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
-    const response = await fetch('http://numbersapi.com/random/math');
+    const response = await fetch('http://localhost:8080');
     const text = await response.text()
 
     return {
