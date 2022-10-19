18a01ed80e2a9f26d72045b98c29886909661e46
Azat Bariev <justlumajustmail@gmail.com>
2022-10-19 17:10:19 +0300

    Update +page.svelte

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -19,10 +19,16 @@
 </script>
 
 <script lang="ts">
-    export let data = [];
+    export let data;
 </script>
 
 
 <h1>Welcome to Cost Accountant!</h1>
 
-
+{#await data}
+    <p>Waiting for a fact...</p>
+{:then data}
+    <p>{data}</p>
+{:catch error}
+    <p>An error occurred!</p>
+{/await}
