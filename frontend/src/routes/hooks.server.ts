bc9f8e2abf8110c27e1559453fbe1e226029515a
Azat Bariev <justlumajustmail@gmail.com>
2022-11-01 21:29:36 +0300

    Update hooks.server.ts

--- frontend/src/routes/hooks.server.ts
+++ frontend/src/routes/hooks.server.ts
@@ -2,6 +2,7 @@ import type { Handle } from '@sveltejs/kit'
 
 export const handle: Handle = async ({ event, resolve }) => {
     // get cookies from browser
+    console.log('Hook works!')
     const session = event.cookies.get('session')
 
     if (!session) {
