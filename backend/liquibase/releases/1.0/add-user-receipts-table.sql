9627da595b10c2db6886b0717d8ce96f22e065f7
Azat Bariev <justlumajustmail@gmail.com>
2022-11-06 00:12:45 +0300

    Update add-user-receipts-table.sql

--- backend/liquibase/releases/1.0/add-user-receipts-table.sql
+++ backend/liquibase/releases/1.0/add-user-receipts-table.sql
@@ -1,8 +1,15 @@
 --liquibase formatted sql
 --changeset ziucay:4
-CREATE TABLE application.user_receipts (
-                                     id SERIAL PRIMARY KEY,
-                                     user_id integer NOT NULL references user(id),
-                                     receipt_id integer NOT NULL references receipt(id)
+CREATE TABLE application.user_receipts
+(
+    id         SERIAL PRIMARY KEY,
+    user_id    integer NOT NULL,
+    receipt_id integer NOT NULL,
+    CONSTRAINT fk_user
+        FOREIGN KEY (user_id)
+            REFERENCES user (id),
+    CONSTRAINT fk_receipt
+        FOREIGN KEY (receipt_id)
+            REFERENCES receipt (id)
 );
 --rollback DROP TABLE application.user_receipts;
