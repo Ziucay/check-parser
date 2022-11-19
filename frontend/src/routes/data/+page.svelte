e6b24c24fb6d01ea8111b104a6661b0cbf8d1963
Azat Bariev <justlumajustmail@gmail.com>
2022-11-19 19:18:17 +0300

    add list view initial

---
+++ frontend/src/routes/data/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import {page} from '$app/stores'
+    import type {PageData} from "../../.svelte-kit/types/src/routes/$types";
+
+    export let data: PageData;
+
+    let receipts = data.list
+
+</script>
+
+{#if $page.data.user}
+    <p>Welcome {$page.data.user.name}!</p>
+{/if}
+
+<p> This is your list of receipts:</p>
+
+<table>
+    {#each receipts as {_id, createdAt, ticket}, i}
+        <tr>
+            <th>_id</th>
+            <th>createdAt</th>
+            <th>ticket.document.receipt.totalSum</th>
+        </tr>
+    {/each}
+</table>
