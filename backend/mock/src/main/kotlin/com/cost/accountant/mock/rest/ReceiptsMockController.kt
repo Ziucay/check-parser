a396ef2a2d45756ec7009b866eead6ef544dd979
Azat Bariev <justlumajustmail@gmail.com>
2022-11-19 18:32:53 +0300

    Update ReceiptsMockController.kt

--- backend/mock/src/main/kotlin/com/cost/accountant/mock/rest/ReceiptsMockController.kt
+++ backend/mock/src/main/kotlin/com/cost/accountant/mock/rest/ReceiptsMockController.kt
@@ -4,7 +4,9 @@ import com.cost.accountant.models.ReceiptEntry
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
+@RestController
 class ReceiptsMockController {
 
     companion object {
