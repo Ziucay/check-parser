d7961f8a7d4a86702e61b9ba26009327a3bdf470
Azat Bariev <justlumajustmail@gmail.com>
2022-11-21 17:57:53 +0300

    make pages dynamic

--- frontend/src/routes/data/[receiptId]/+page.server.ts
+++ frontend/src/routes/data/[receiptId]/+page.server.ts
@@ -1,13 +1,13 @@
 import { redirect } from '@sveltejs/kit'
 import type { PageServerLoad } from './$types'
 
-export const load: PageServerLoad = async ({ locals }) => {
+export const load: PageServerLoad = async ({ locals, params }) => {
     // redirect user if not logged in
     if (!locals.user) {
         throw redirect(302, '/')
     }
 
-    const response = await fetch('http://mock:8080/mock/receipt?id=3');
+    const response = await fetch(`http://mock:8080/mock/receipt?id=${params.receiptId}`);
     const list = await response.json()
 
     return {
