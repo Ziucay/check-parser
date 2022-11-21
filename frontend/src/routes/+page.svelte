a33f48e5fb9a6de13e5bd4a67099d18dc7441dba
Azat Bariev <justlumajustmail@gmail.com>
2022-11-21 18:59:04 +0300

    Update +page.svelte

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
     import type {PageData} from "../../.svelte-kit/types/src/routes/$types";
-    import { writable } from "svelte/store";
     import {redirect} from "@sveltejs/kit";
 
 
     export let data: PageData;
-
-    const store = writable(localStorage.getItem("json") || "");
 
     let fileInput;
 
