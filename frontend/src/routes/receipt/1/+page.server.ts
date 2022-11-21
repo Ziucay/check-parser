afd72473d8f2fb58c8852be1d1a1141613aa2c50
Hallejujah123 <zhukov-02@yandex.ru>
2022-11-21 17:26:52 +0300

    receipt

---
+++ frontend/src/routes/receipt/1/+page.server.ts
@@ -0,0 +1,16 @@
+import { redirect } from '@sveltejs/kit'
+import type { PageServerLoad } from './$types'
+
+export const load: PageServerLoad = async ({ locals }) => {
+    // redirect user if not logged in
+    if (!locals.user) {
+        throw redirect(302, '/')
+    }
+
+    const response = await fetch('http://mock:8080/mock/receipt/1');
+    const list = await response.json()
+
+    return {
+        list: list
+    };
+}
---
+++ frontend/src/routes/receipt/2/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import {page} from '$app/stores'
+    import type {PageData} from "../../.svelte-kit/types/src/routes/$types";
+
+    export let data: PageData;
+
+    let receipt = data.list
+</script>
+
+{#if $page.data.user}
+    <p>Welcome {$page.data.user.name}!</p>
+{/if}
+
+<p> This is your receipt:</p>
+
+{#each receipt as {ticket}, i}
+    <p>{ticket.document.items[i].name} {ticket.document.items[i].quantity} {ticket.document.items[i].sum}</p>
+{/each}
+
+<p>Дата: {receipt.ticket.document.receipt.dateTime}</p>
+<p>Общая сумма: {receipt.ticket.document.receipt.totalSum}</p>
