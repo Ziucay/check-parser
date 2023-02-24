793b2f51229afda0771a44dc3f51965b18665ade
Azat Bariev <justlumajustmail@gmail.com>
2023-02-24 19:54:31 +0300

    fix hosts

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -1,7 +1,7 @@
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
-	const response = await fetch('http://mock:8080/mock/receipts/list');
+	const response = await fetch('http://back:8080/mock/receipts/list');
 	const text = await response.text();
 
 	return {
