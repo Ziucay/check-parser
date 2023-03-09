ed6dc3a33abba3111810cc518b76812c7a5bff67
Azat Bariev <justlumajustmail@gmail.com>
2023-03-09 17:24:30 +0300

    Update +page.server.ts

--- frontend/src/routes/login/+page.server.ts
+++ frontend/src/routes/login/+page.server.ts
@@ -34,8 +34,7 @@ const login: Action = async ({ cookies, request }) => {
 	if (retrievedText === 'error') {
 		return invalid(400, { credentials: true });
 	}
-    console.log(retrievedText)
-    console.log(password)
+    
 	const userPasswordEqual = await bcrypt.compare(password, retrievedText);
 
 	if (!userPasswordEqual) {
